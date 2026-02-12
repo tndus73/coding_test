a5bc9f1535ddf08c4e505fb18b1e1026566616a8
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-02-12 15:43:32 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,16 +1,17 @@
 -- 코드를 입력하세요
+-- 코드를 입력하세요
 with car_rental
 as
 (
 SELECT CAR_ID
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-where START_DATE >= to_date('2022-08-01','YYYY-MM-DD') and START_DATE < to_date('2022-11-01','YYYY-MM-DD')
+where START_DATE >= '2022-08-01' and START_DATE < '2022-11-01'
 group by CAR_ID having count(*)>=5
 )
 
-select to_char(START_DATE,'MM')*1.0 month, a.CAR_ID, count(*) records
+select date_format(START_DATE,'%m')*1.0 month, a.CAR_ID, count(*) records
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY a
 inner join car_rental b on a.CAR_ID=b.CAR_ID
-where START_DATE >= to_date('2022-08-01','YYYY-MM-DD') and START_DATE < to_date('2022-11-01','YYYY-MM-DD')
-group by to_char(START_DATE,'MM'), a.CAR_ID having count(*)>0
+where START_DATE >= '2022-08-01' and START_DATE < '2022-11-01'
+group by date_format(START_DATE,'%m'), a.CAR_ID having count(*)>0
 order by month, CAR_ID desc
