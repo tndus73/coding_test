bade8065bd29a552d1b499dbdd3ce213a21271b3
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-02-13 16:26:56 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/README.md
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 5] 상품을 구매한 회원 비율 구하기 - 131534 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131534#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131534?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2026년 02월 12일 00:59:41
+2026년 02월 13일 16:26:54
 
 ### 문제 설명
 
--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,28 +1,23 @@
 -- 코드를 입력하세요
-with user_2021
-as
+with T_2021 as
 (
-select USER_ID
+SELECT USER_ID
 from USER_INFO
-where JOINED >= TO_DATE('2021-01-01','YYYY-MM-DD')
-and JOINED < TO_DATE('2022-01-01','YYYY-MM-DD')
+where JOINED >= '2021-01-01' and JOINED < '2022-01-01'
 )
-,   T_user
-AS
+,   T_cnt as
 (
-  SELECT COUNT(*) total_cnt
-  FROM user_2021
-)
-,   T_product
-as
-(
-select to_char(SALES_DATE,'YYYY') Year, to_char(SALES_DATE,'MM')*1 Month, count(distinct a.USER_ID) Pur_user
-from ONLINE_SALE a
-left join user_2021 b on a.USER_ID = b.USER_ID
-where b.USER_ID is not null   
-group by to_char(SALES_DATE,'YYYY'), to_char(SALES_DATE,'MM')*1
+SELECT count(distinct USER_ID) total_user
+from T_2021
 )
 
-select year, month, pur_user PURCHASED_USERS, round(pur_user/total_cnt,1) PUCHASED_RATIO
-from T_product, T_user
-order by Year, Month
+select year(SALES_DATE) year, month(SALES_DATE)*1 month , count(distinct a.USER_ID) PURCHASED_USERS
+        , round(count(distinct a.USER_ID)*1/total_user,1) PUCHASED_RATIO
+from ONLINE_SALE a , T_cnt b
+    
+where USER_ID in (select USER_ID
+                from T_2021)
+group by  year(SALES_DATE), month(SALES_DATE)*1    
+order by year, month
+
+
