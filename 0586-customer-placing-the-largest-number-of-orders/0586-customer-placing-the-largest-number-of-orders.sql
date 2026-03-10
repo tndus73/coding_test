600c64230dc202bd71faf26c9c864eb16748ce12
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-10 19:14:36 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
+/**
+select customer_number
+from Orders
+group by customer_number
+order by count(order_number) desc
+limit 1;
+**/
 select customer_number
 from (
     select customer_number, count(*) cnt
