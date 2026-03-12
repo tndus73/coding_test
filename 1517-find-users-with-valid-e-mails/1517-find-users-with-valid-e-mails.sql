66fc3515059d8e5e779f1b417ce1b59e1be40908
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-13 01:24:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 select user_id, name, mail
 from Users
-where mail like BINARY '%@leetcode.com' and mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
+where mail like BINARY '%@leetcode.com' --binary쓰면 정확히 대소문자 구분함.
+and mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
