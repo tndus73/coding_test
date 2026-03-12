b3964201a3d3d8df463c9d61f15a8720f68856c7
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-13 01:33:28 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/** COUNT(v.visit_id) 써도 null count잘함 **/
 select customer_id, sum(ifnull(T.transaction_id,1)) count_no_trans
 from Visits V 
 left join Transactions T on V.visit_id=T.visit_id
