db74ea3d50d30399fae5f8d63eaaaf3fd938cc0e
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-13 01:53:54 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/** CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2))) **/
 select user_id, concat(upper(left(name,1)),lower(substring(name,2))) name
 from Users
 order by user_id
