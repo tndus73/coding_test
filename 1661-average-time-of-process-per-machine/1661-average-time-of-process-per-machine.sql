60d8183d4aa6288d926c004e749222d42578b971
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-13 23:45:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,4 +1,16 @@
 # Write your MySQL query statement below
+/**
+SELECT machine_id,
+       ROUND(AVG(process_time),3) AS processing_time
+FROM (
+    SELECT machine_id,
+           process_id,
+           MAX(timestamp) - MIN(timestamp) AS process_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+) t
+GROUP BY machine_id;
+**/
 with ai
 as
 (
