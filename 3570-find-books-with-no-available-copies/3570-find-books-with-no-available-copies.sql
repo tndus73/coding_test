380aea46ba1611f8729de00e177db3bc64d15c7d
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-14 02:17:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/**
 select  book_id, title, author,  genre,  publication_year, current_borrowers
 from (
     select I.book_id, title, author,  genre,  publication_year, count(B.book_id) current_borrowers
@@ -8,4 +9,11 @@ from (
     where return_date is null
     group by I.book_id) T
 where cur =0
+order by  current_borrowers desc, title 
+**/
+select I.book_id, title, author,  genre,  publication_year, count(B.book_id) current_borrowers
+from library_books I
+left join borrowing_records B on I.book_id=B.book_id
+where return_date is null
+group by I.book_id having max(total_copies) - count(B.book_id) =0
 order by  current_borrowers desc, title 
