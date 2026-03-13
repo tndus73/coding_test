0779d8c9f8b61e594443379729a2892abf73e12f
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-14 03:08:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
+/**
 select distinct num ConsecutiveNums 
 from (
 select num,  id - DENSE_RANK() OVER(PARTITION BY num ORDER BY id) AS grp
 from Logs ) T
 group by num, grp  having count(*)>=3
+**/
+select distinct num ConsecutiveNums 
+from(
+select num, lag(num,1) over(order by id) lag1
+        , lag(num,2) over(order by id) lag2
+from Logs)T
+where num=lag1 and num=lag2
