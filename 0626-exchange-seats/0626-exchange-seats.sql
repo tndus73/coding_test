cd948b1a5f2eeab9cd7daab96ed81ed2f77b79d6
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-14 23:31:21 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,10 +1,20 @@
 # Write your MySQL query statement below
-
+/**
 SELECT id,
     CASE
         WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN student
         WHEN id % 2 = 1 THEN LEAD(student) OVER (ORDER BY id)
         ELSE LAG(student) OVER (ORDER BY id)
     END AS student
+FROM Seat
+ORDER BY id;
+**/
+
+SELECT
+    CASE WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1
+        END AS id
+    , student
 FROM Seat
 ORDER BY id;
