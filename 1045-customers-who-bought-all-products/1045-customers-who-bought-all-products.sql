be165344a301f9c9665fb37ffc7b76d3cac8b897
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-15 01:35:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,11 +1,9 @@
 # Write your MySQL query statement below
-with id_pr
-as
-(
-select customer_id, count(distinct product_key) prodct_key
-from Customer C
-group by customer_id
-)
+
 select customer_id
-from id_pr
+from (
+    select customer_id, count(distinct product_key) prodct_key
+    from Customer C
+    group by customer_id
+    )
 where prodct_key in (select count(*) from Product P)
