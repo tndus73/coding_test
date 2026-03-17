876a679da67f3061dd7db92dd8c3c58969b0f732
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-18 03:55:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -1,4 +1,17 @@
 # Write your MySQL query statement below
+/**
+select 
+    user_id,
+    round(avg(case when activity_type='free_trial' then activity_duration end),2) as trial_avg_duration,
+    round(avg(case when activity_type='paid' then activity_duration end),2) as paid_avg_duration
+from 
+    UserActivity
+group by user_id 
+having AVG(CASE WHEN activity_type = 'paid' THEN activity_duration END) IS NOT NULL
+order by 
+    user_id;
+**/
+
 with total
 as
 (
