34b4b2529b5a39918bcfb589d9ed21b9efd53cd3
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-18 05:01:28 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
+++ 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
@@ -2,27 +2,21 @@
 with total
 as
 (
-select S.product_id,  case when month(sale_date) in (12, 1, 2) then 'Winter'
-                            when month(sale_date) in (3, 4, 5) then 'Spring'
-                            when month(sale_date) in (6, 7, 8) then 'Summer'
-                            when month(sale_date) in (9, 10, 11) then 'Fall'
-                            end season
-       , category, quantity, quantity*price revenue                     
+select case when month(sale_date) in (12, 1, 2) then 'Winter'
+            when month(sale_date) in (3, 4, 5) then 'Spring'
+            when month(sale_date) in (6, 7, 8) then 'Summer'
+            when month(sale_date) in (9, 10, 11) then 'Fall'
+            end season
+       , category,  sum(quantity) total_quantity, sum(quantity*price) total_revenue                    
 from sales S
 left join products P on S.product_id=P.product_id
-)
-,   total2
-as
-(
-select season, category, sum(quantity) total_quantity, sum(revenue) total_revenue
-from total
 group by season, category
-order by season
 )
+
 select season, category, total_quantity, total_revenue
 from(
 select season, category, total_quantity, total_revenue
         , rank() over (partition by season order by total_quantity desc, total_revenue desc) rn
-from total2) T
+from total) T
 where rn = 1
 order by 1
