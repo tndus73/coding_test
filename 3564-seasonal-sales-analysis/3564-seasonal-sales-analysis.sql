c220ce971b019e238e98cafc68779777aa7c4020
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-18 05:02:14 +0900

    Time: 503 ms (68.86%), Space: 0B (100%) - LeetHub

--- 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
+++ 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
@@ -7,10 +7,9 @@ select S.product_id,  case when month(sale_date) in (12, 1, 2) then 'Winter'
                             when month(sale_date) in (6, 7, 8) then 'Summer'
                             when month(sale_date) in (9, 10, 11) then 'Fall'
                             end season
-       , category,  sum(quantity) quantity, quantity*price revenue                     
+       , category,  quantity quantity, quantity*price revenue                     
 from sales S
 left join products P on S.product_id=P.product_id
-group by season, category
 )
 ,   total2
 as
