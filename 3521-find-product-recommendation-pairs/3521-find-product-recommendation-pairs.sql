52b32cd8e92ecb60683db19a8b8c7d0f088e533e
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-18 04:28:48 +0900

    Time: 638 ms (32.93%), Space: 0B (100%) - LeetHub

---
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+with product_purch
+as
+(
+select user_id, PP.product_id, category
+from ProductPurchases PP
+inner join ProductInfo P on PP.product_id = P.product_id
+)
+,   total
+as
+(
+select P1.user_id, P1.product_id product1_id, P1.category product1_category
+        , P2.product_id product2_id, P2.category product2_category
+from product_purch P1
+inner join product_purch P2 on P1.user_id=P2.user_id and P1.product_id < P2.product_id
+)
+select product1_id, product2_id, product1_category, product2_category, count(distinct user_id) customer_count
+from total
+group by product1_id, product2_id, product1_category, product2_category having count(distinct user_id)>2
+order by customer_count desc, product1_id, product2_id
