f69cb9cca393199de4c19a7cdb38b93660622819
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-18 04:31:01 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -1,4 +1,19 @@
 # Write your MySQL query statement below
+/**
+select 
+    p1.product_id as product1_id,
+    p2.product_id as product2_id,
+    i.category as product1_category,
+    i2.category as product2_category,
+    count(*) as customer_count
+from ProductPurchases p1
+    left join ProductPurchases p2 on p1.user_id = p2.user_id and p1.product_id < p2.product_id
+    left join productinfo i on p1.product_id = i.product_id
+    left join productinfo i2 on p2.product_id = i2.product_id
+group by p1.product_id, p2.product_id
+having count(*) >= 3 and product2_id is not null
+order by customer_count desc, product1_id, product2_id
+**/
 with product_purch
 as
 (
@@ -16,5 +31,5 @@ inner join product_purch P2 on P1.user_id=P2.user_id and P1.product_id < P2.prod
 )
 select product1_id, product2_id, product1_category, product2_category, count(distinct user_id) customer_count
 from total
-group by product1_id, product2_id, product1_category, product2_category having count(distinct user_id)>2
+group by product1_id, product2_id, product1_category, product2_category having count(distinct user_id)>=3
 order by customer_count desc, product1_id, product2_id
