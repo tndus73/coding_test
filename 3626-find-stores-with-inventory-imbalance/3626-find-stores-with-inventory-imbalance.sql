f52dde1f471a6a76c27092a9ca295eaedfec703d
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-20 16:49:42 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3626-find-stores-with-inventory-imbalance/3626-find-stores-with-inventory-imbalance.sql
+++ 3626-find-stores-with-inventory-imbalance/3626-find-stores-with-inventory-imbalance.sql
@@ -1,4 +1,16 @@
 # Write your MySQL query statement below
+/**
+summary AS (
+    SELECT
+        store_id,
+        MAX(CASE WHEN maxP = 1 THEN product_name END) AS most_exp_product,
+        MAX(CASE WHEN maxP = 1 THEN quantity END) AS max_qty,
+        MAX(CASE WHEN minP = 1 THEN product_name END) AS cheapest_product,
+        MAX(CASE WHEN minP = 1 THEN quantity END) AS min_qty
+    FROM total
+    GROUP BY store_id
+)
+**/
 with total
 as
 (
@@ -10,24 +22,11 @@ where store_id in (select store_id
                     from inventory
                     group by store_id having count(distinct product_name)>=3)
 )
-, maxpr
-as
-(
-select store_id, product_name, quantity
-from total
-where maxP=1
-)
-, minpr
-as
-(
-select store_id, product_name, quantity
-from total
-where minP=1
-)
+
 select X.store_id, store_name, location, X.product_name most_exp_product
         , I.product_name cheapest_product
         , round(I.quantity/X.quantity,2) imbalance_ratio
-from maxpr X
-inner join minpr I on X.store_id=I.store_id and X.quantity<I.quantity
+from total X
+inner join total I on X.store_id=I.store_id and X.quantity<I.quantity and X.maxp=1 and I.minp=1
 inner join stores S on X.store_id=S.store_id 
 order by imbalance_ratio desc, store_name
