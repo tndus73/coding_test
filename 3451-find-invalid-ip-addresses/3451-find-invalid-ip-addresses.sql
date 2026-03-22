2b83cd3d2f5bad9dc0ea9432d738eb7352ebbaa3
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-23 02:13:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3451-find-invalid-ip-addresses/3451-find-invalid-ip-addresses.sql
+++ 3451-find-invalid-ip-addresses/3451-find-invalid-ip-addresses.sql
@@ -1,14 +1,5 @@
-WITH
-  cte_invalid_ip AS (
-    SELECT log_id, ip
-    FROM logs
-    WHERE NOT regexp_like(ip, "^(?:[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:[.](?:[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$")
-  ),
-  cte_invalid_ip_count AS (
-    SELECT ip, count(log_id) 'invalid_count'
-    FROM cte_invalid_ip
-    GROUP BY ip
-  )
-SELECT ip, invalid_count
-FROM cte_invalid_ip_count
+SELECT ip , COUNT(*) as invalid_count
+FROM logs 
+WHERE ip NOT REGEXP '^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|0)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|0)$'
+GROUP BY ip
 ORDER BY invalid_count DESC, ip DESC;
