3c1b987385ab275325215ac90cec92d9af0f9ba8
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-25 18:27:25 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,21 +1,46 @@
--- 코드를 입력하세요
-with T_2021 as
+/**-- 코드를 입력하세요
+with user_2021
+as
 (
-SELECT USER_ID
+select USER_ID
 from USER_INFO
-where JOINED >= '2021-01-01' and JOINED < '2022-01-01'
+where JOINED >= TO_DATE('2021-01-01','YYYY-MM-DD')
+and JOINED < TO_DATE('2022-01-01','YYYY-MM-DD')
 )
-,   T_cnt as
+,   T_user
+AS
 (
-SELECT count(distinct USER_ID) total_user
-from T_2021
+  SELECT COUNT(*) total_cnt
+  FROM user_2021
+)
+,   T_product
+as
+(
+select to_char(SALES_DATE,'YYYY') Year, to_char(SALES_DATE,'MM')*1 Month, count(distinct a.USER_ID) Pur_user
+from ONLINE_SALE a
+left join user_2021 b on a.USER_ID = b.USER_ID
+where b.USER_ID is not null   
+group by to_char(SALES_DATE,'YYYY'), to_char(SALES_DATE,'MM')*1
 )
 
-select year(SALES_DATE) year, month(SALES_DATE)*1 month , count(distinct a.USER_ID) PURCHASED_USERS
-        , round(count(distinct a.USER_ID)*1/total_user,1) PUCHASED_RATIO
-from ONLINE_SALE a , T_cnt b
-    
-where USER_ID in (select USER_ID
-                from T_2021)
-group by  year(SALES_DATE), month(SALES_DATE)*1    
-order by year, month
+select year, month, pur_user PURCHASED_USERS, round(pur_user/total_cnt,1) PUCHASED_RATIO
+from T_product, T_user
+order by Year, Month
+**/
+
+SELECT TO_CHAR(O.SALES_DATE, 'YYYY') AS YEAR,
+       TO_NUMBER(TO_CHAR(O.SALES_DATE, 'MM')) AS MONTH,
+       COUNT(DISTINCT O.USER_ID) AS PURCHASED_USERS,
+       ROUND(COUNT(DISTINCT O.USER_ID)*1 / (
+               SELECT COUNT(*)
+               FROM USER_INFO
+               WHERE JOINED >= DATE '2021-01-01'
+                 AND JOINED < DATE '2022-01-01'
+           ),1) AS PURCHASED_RATIO
+FROM USER_INFO U
+JOIN ONLINE_SALE O ON U.USER_ID = O.USER_ID
+WHERE U.JOINED >= DATE '2021-01-01'
+  AND U.JOINED < DATE '2022-01-01'
+GROUP BY TO_CHAR(O.SALES_DATE, 'YYYY'),
+         TO_CHAR(O.SALES_DATE, 'MM')
+ORDER BY YEAR, MONTH;
