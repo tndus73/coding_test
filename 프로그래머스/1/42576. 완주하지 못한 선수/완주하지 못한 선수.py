27031d0f5733f386bfb8eed076482af743c14725
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-30 03:23:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.74 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,4 @@
+'''정렬
 def solution(participant, completion):
     participant.sort()
     completion.sort()
@@ -5,4 +6,13 @@ def solution(participant, completion):
     for x, y in zip(participant, completion):
         if x != y:
             return x
-    return participant[-1]
+    return participant[-1] #완주못한 선수가 제일마지막에 있을때
+'''
+# 해시
+from collections import Counter
+
+def solution(participant, completion):
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]
+    
+    
