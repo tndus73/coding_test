284b075009d3af826d074a785c4aa5cb7d877113
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-30 01:48:05 +0900

    [level 1] Title: 모의고사, Time: 4.09 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,22 @@
 def solution(answers):
-    A=0
-    B=0
-    C=0
-    p1 = [1,2,3,4,5]
-    p2 = [2,1,2,3,2,4,2,5]
-    p3 = [3,3,1,1,2,2,4,4,5,5]
-    for i in range(len(answers)):
-        if answers[i] == p1[i%(len(p1))]:
-            A+=1
-        if answers[i] == p2[i%(len(p2))]:
-            B+=1
-        if answers[i] == p3[i%(len(p3))]:
-            C+=1    
     answer = []
-    if A == max(A,B,C):
+    A = [1,2,3,4,5]
+    B = [2,1,2,3,2,4,2,5]
+    C = [3,3,1,1,2,2,4,4,5,5]
+    Acnt = 0
+    Bcnt = 0
+    Ccnt = 0
+    for i in range(len(answers)):
+        if A[i%len(A)] == answers[i]:
+            Acnt +=1
+        if B[i%len(B)] == answers[i]:
+            Bcnt +=1
+        if C[i%len(C)] == answers[i]:
+            Ccnt +=1    
+    if max(Acnt,Bcnt,Ccnt) == Acnt:
         answer.append(1)
-    if B == max(A,B,C):
-        answer.append(2)
-    if C == max(A,B,C):
-        answer.append(3)    
+    if  max(Acnt,Bcnt,Ccnt) == Bcnt:
+        answer.append(2)   
+    if max(Acnt,Bcnt,Ccnt) == Ccnt:
+        answer.append(3)        
     return answer
