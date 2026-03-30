531e451f5ffe728b8991bede0a7a948f9f9807e2
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-31 01:25:53 +0900

    [level 2] Title: 가장 큰 수, Time: 1276.53 ms, Memory: 26.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,13 @@
 '''
 i를 문자로 바꾸고 x*3트릭 쓴 후 sort(reverse=true)해서 붙인다
-'''
 
 def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x*3, reverse=True)
     return str(int(''.join(numbers)))
+'''
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    k = sorted(numbers, key=lambda x: x*3, reverse=True)
+    return str(int(''.join(k)))
