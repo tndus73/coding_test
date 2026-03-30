30d7a597be92516b94251ef8490138f550658db0
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-31 06:28:18 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-
+/**
 with R_CNT
 as
 (
@@ -9,7 +9,7 @@ from (
             , rank() over (order by count(MEMBER_ID) desc) rn
     from REST_REVIEW
     group by MEMBER_ID
-    ) t
+    ) t -- mysql은 서브쿼리에 as명칭 해줘야함
 where rn=1    
 ) 
 
@@ -17,4 +17,19 @@ select MEMBER_NAME, REVIEW_TEXT, date_format(a.REVIEW_DATE,'%Y-%m-%d') REVIEW_DA
 from REST_REVIEW a
 inner join R_CNT b on a.MEMBER_ID = b.MEMBER_ID
 inner join MEMBER_PROFILE c on a.MEMBER_ID = c.MEMBER_ID
+order by REVIEW_DATE, REVIEW_TEXT
+**/
+with manyr
+as
+(
+select MEMBER_ID, dense_rank() over(order by count(*) desc) rn
+from REST_REVIEW
+group by MEMBER_ID
+)
+
+select MEMBER_NAME,	REVIEW_TEXT, DATE_FORMAT(R.REVIEW_DATE, '%Y-%m-%d')
+from MEMBER_PROFILE M 
+inner join manyr T on M.MEMBER_ID=T.MEMBER_ID
+inner join REST_REVIEW R on M.MEMBER_ID=R.MEMBER_ID
+where rn = 1
 order by REVIEW_DATE, REVIEW_TEXT
