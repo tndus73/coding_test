da3eea291f41339e6c9a406aa48099b6eaada152
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-31 00:15:47 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 52.16 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,19 +1,19 @@
 from collections import deque
-
+#다리에 올라갈 수 있는 트럭 수 bridge_length, 다리가 견딜 수 있는 무게 weight, 트럭 별 무게 truck_weights
 def solution(bridge_length, weight, truck_weights):
     bridge = deque([0] * bridge_length)
     trucks = deque(truck_weights)
-    bridge_sum = 0
+    bridsum = 0
     time = 0
 
-    while trucks or bridge_sum > 0:
+    while trucks or bridsum > 0:
         time += 1
-        bridge_sum -= bridge.popleft()
+        bridsum -= bridge.popleft()
 
-        if trucks and bridge_sum + trucks[0] <= weight:
+        if trucks and bridsum + trucks[0] <= weight:
             truck = trucks.popleft()
             bridge.append(truck)
-            bridge_sum += truck
+            bridsum += truck
         else:
             bridge.append(0)
 
