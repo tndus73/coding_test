2e258b1f17678f46c67e8e85516518875b763486
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-03-31 06:53:20 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,46 +1,34 @@
-/**-- 코드를 입력하세요
-with user_2021
-as
+-- 코드를 입력하세요
+/**
+with T_2021 as
 (
-select USER_ID
+SELECT USER_ID
 from USER_INFO
-where JOINED >= TO_DATE('2021-01-01','YYYY-MM-DD')
-and JOINED < TO_DATE('2022-01-01','YYYY-MM-DD')
+where JOINED >= '2021-01-01' and JOINED < '2022-01-01'
 )
-,   T_user
-AS
+,   T_cnt as
 (
-  SELECT COUNT(*) total_cnt
-  FROM user_2021
-)
-,   T_product
-as
-(
-select to_char(SALES_DATE,'YYYY') Year, to_char(SALES_DATE,'MM')*1 Month, count(distinct a.USER_ID) Pur_user
-from ONLINE_SALE a
-left join user_2021 b on a.USER_ID = b.USER_ID
-where b.USER_ID is not null   
-group by to_char(SALES_DATE,'YYYY'), to_char(SALES_DATE,'MM')*1
+SELECT count(distinct USER_ID) total_user
+from T_2021
 )
 
-select year, month, pur_user PURCHASED_USERS, round(pur_user/total_cnt,1) PUCHASED_RATIO
-from T_product, T_user
-order by Year, Month
+select year(SALES_DATE) year, month(SALES_DATE)*1 month , count(distinct a.USER_ID) PURCHASED_USERS
+        , round(count(distinct a.USER_ID)*1/total_user,1) PUCHASED_RATIO
+from ONLINE_SALE a , T_cnt b
+    
+where USER_ID in (select USER_ID
+                from T_2021)
+group by  year(SALES_DATE), month(SALES_DATE)*1    
+order by year, month
 **/
 
-SELECT TO_CHAR(O.SALES_DATE, 'YYYY') AS YEAR,
-       TO_NUMBER(TO_CHAR(O.SALES_DATE, 'MM')) AS MONTH,
-       COUNT(DISTINCT O.USER_ID) AS PURCHASED_USERS,
-       ROUND(COUNT(DISTINCT O.USER_ID)*1 / (
-               SELECT COUNT(*)
-               FROM USER_INFO
-               WHERE JOINED >= DATE '2021-01-01'
-                 AND JOINED < DATE '2022-01-01'
-           ),1) AS PURCHASED_RATIO
-FROM USER_INFO U
-JOIN ONLINE_SALE O ON U.USER_ID = O.USER_ID
-WHERE U.JOINED >= DATE '2021-01-01'
-  AND U.JOINED < DATE '2022-01-01'
-GROUP BY TO_CHAR(O.SALES_DATE, 'YYYY'),
-         TO_CHAR(O.SALES_DATE, 'MM')
-ORDER BY YEAR, MONTH;
+select year(O.SALES_DATE) year, MONTH(O.SALES_DATE) MONTH
+        , count(distinct O.USER_ID) PURCHASED_USERS
+        , round(count(distinct O.USER_ID) / (select count(*)
+                                                from USER_INFO 
+                                                where year(JOINED)=2021),1) PUCHASED_RATIO
+from USER_INFO U
+inner join ONLINE_SALE O on U.USER_ID=O.USER_ID
+where year(JOINED)=2021
+group by year(O.SALES_DATE),	MONTH(O.SALES_DATE)
+order by year, month
