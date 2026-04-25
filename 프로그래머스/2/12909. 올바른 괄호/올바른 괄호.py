3336e6f5a64b13b0fd2c7e6160d39e61e472f570
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-25 17:57:38 +0900

    [level 2] Title: 올바른 괄호, Time: 9.59 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,29 +1,10 @@
-'''
-()개수가 같아야함
 def solution(s):
-    count = 0
-
-    for ch in s:
-        if ch == '(':
-            count += 1
+    arr = []
+    for i in s:
+        if i == '(':
+            arr.append(i)
         else:
-            count -= 1
-
-        if count < 0:
-            return False
-
-    return count == 0
-'''
-
-def solution(s):
-    stack = []
-    
-    for x in s:
-        if x == "(":
-            stack.append(x)
-        else:
-            if not stack:
+            if not arr:
                 return False
-            stack.pop()
-    
-    return len(stack) == 0
+            arr.pop()
+    return len(arr) == 0        
