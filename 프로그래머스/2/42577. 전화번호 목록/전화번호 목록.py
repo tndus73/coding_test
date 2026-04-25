d6bfddc0d97c6035fa6687fc044a1fc2852a8346
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-26 02:51:16 +0900

    [level 2] Title: 전화번호 목록, Time: 101.08 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,6 @@
-'''전화번호가 다른 번호의 접두사인 경우 False'''
 def solution(phone_book):
     phone_book.sort()
-    
     for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
+        if phone_book[i+1][:len(phone_book[i])] == phone_book[i]:
             return False
-    return True 
-    
+    return True    
