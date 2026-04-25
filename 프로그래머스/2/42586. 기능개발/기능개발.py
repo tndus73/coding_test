51692918fa4262ea93f66778ffac235387a2bf79
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-25 18:41:52 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,48 +1,22 @@
-'''
-개발속도는 다르지만, 배포는 앞에있는 기능 배포될때 뒤에 있는 기능 배포
-먼저 배포되는 순서대로 작업 진도 정수배열 progresses
-각 작업의 개발 속도 정수배열 speeds
-각 배포마다 몇개의 기능이 배포되는지 return
-import math
-
 def solution(progresses, speeds):
-    days = []
-    
-    for pro, spe in zip(progresses, speeds):
-            days.append(math.ceil((100-pro)/spe))
-            
-    answer = []
-    current = days[0]
-    count = 1
-    
-    for i in range(1, len(days)):
-        if days[i] <= current:
-            count +=1
-        else:
-            answer.append(count)
-            current = days[i]
-            count = 1
-            
-    answer.append(count)
-    return answer
-'''
-def solution(progresses, speeds):
+        arr = []
         answer = []
-        days=[]
-    
-        for p, s in zip(progresses, speeds):
-            day = (100-p+s-1)//s
-            days.append(day)
-            
-        current = days[0]
-        count=1    
-        
-        for i in range(1, len(days)):
-            if days[i] <= current:
-                    count +=1
+
+        for pro, spe in zip(progresses,speeds):
+            if (100-pro)%spe == 0:
+                arr.append((100-pro)//spe)
             else:
-                    answer.append(count)
-                    current = days[i]
-                    count = 1
-        answer.append(count)
+                arr.append((100-pro)//spe+1)
+        
+        standard = arr[0]
+        count = 1
+        
+        for i in range(1,len(arr)):
+            if arr[i] <= standard:
+                count+=1
+            else:
+                answer.append(count)
+                standard = arr[i]
+                count = 1
+        answer.append(count)    
         return answer
