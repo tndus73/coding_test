400b6b87f3feb06d675daf3dee03af5c324e44ca
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-26 22:56:00 +0900

    [level 2] Title: 카펫, Time: 163.18 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,7 @@
-# width * height = brown + yellow
-# (width-2) * (height-2) = yellow
 def solution(brown, yellow):
-    total = brown+yellow
-    #세로 길이 탐색
-    for height in range(1, total+1):
-        if total % height == 0:
-            width = total//height
-            if (width-2)*(height-2) == yellow:
-                return [width,height]
+    for a in range(3, brown+yellow+1): #가로
+        for b in range(3, a+1): #세로
+            if a * b== brown+yellow:
+                if yellow == (a-2) * (b-2):
+                     return [a,b]
+
