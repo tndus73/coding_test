b2eb0fda5955bd80e71b48d519b1cb1bcf8a13c2
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-26 17:54:50 +0900

    [level 2] Title: 피로도, Time: 63.26 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,16 +1,18 @@
 def solution(k, dungeons):
+    visited = [False] * len(dungeons)
     
-    def dfs(k, visited):
+    def dfs(fatigue, visited):
         max_count = 0
 
         for i in range(len(dungeons)):
-            if not visited[i] and k >= dungeons[i][0]:
-                visited[i] = True
-                count = 1 + dfs(k - dungeons[i][1], visited)
-                max_count = max(max_count, count)
-                visited[i] = False
-
-        return max_count
-
-    visited = [False] * len(dungeons)
+            if not visited[i]:
+                need, cost = dungeons[i]
+                
+                if fatigue >= need:
+                    visited[i] = True
+                    count = 1+dfs(fatigue-cost, visited)
+                    visited[i] = False
+                    
+                    max_count = max(max_count, count)
+        return max_count                
     return dfs(k, visited)
