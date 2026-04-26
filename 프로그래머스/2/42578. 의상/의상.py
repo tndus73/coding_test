8510aa9eaddb501db1ba00dd78446a266c4ecaa5
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-26 19:42:40 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,13 @@
 def solution(clothes):
-    clothes_dict = {}
-    
-    for name, kind in clothes:
-        if kind in clothes_dict:
-            clothes_dict[kind] += 1
-        else:
-            clothes_dict[kind] = 1
-    
+    dic = {}
     answer = 1
+    for name, kind in clothes:
+        if kind in dic:
+            dic[kind] +=1
+        else:
+            dic[kind] = 1
     
-    for count in clothes_dict.values():
-        answer *= (count+1)
-    return answer-1
+    for kind in dic:
+        answer *= (dic[kind]+1)
+    
+    return answer -1
