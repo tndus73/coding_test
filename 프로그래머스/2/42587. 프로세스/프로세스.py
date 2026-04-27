13765edb18b7f08ae43db7dba79e21f435e8695c
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-28 01:32:11 +0900

    [level 2] Title: 프로세스, Time: 0.57 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,19 +1,17 @@
 from collections import deque
-
 def solution(priorities, location):
     queue = deque()
-    for i, p in enumerate(priorities):
-        queue.append((p,i))
-    
+    for idx, num in enumerate(priorities):
+        queue.append((idx, num))
     count = 0
     while queue:
-        cur_p, cur_i = queue.popleft()
-        
-        if any(cur_p < p for p, _ in queue):
-                queue.append((cur_p, cur_i))
+        cur_i, cur_p = queue.popleft()
+        if any(cur_p < p for i,p in queue):
+            queue.append((cur_i,cur_p))
         else:
-                count += 1
-                if cur_i ==location:
-                    return count
+            count += 1
+            if cur_i == location:
+                return count
+
     
     
