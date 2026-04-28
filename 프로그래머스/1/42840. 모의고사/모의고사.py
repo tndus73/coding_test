3fdfccf0d3001d0273ab036caf7cf1916bab8d45
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-28 17:16:10 +0900

    [level 1] Title: 모의고사, Time: 3.47 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -2,21 +2,22 @@ def solution(answers):
     answer = []
     A = [1,2,3,4,5]
     B = [2,1,2,3,2,4,2,5]
-    C = [3,3,1,1,2,2,4,4,5,5]
-    Acnt = 0
-    Bcnt = 0
-    Ccnt = 0
+    C = [3,3,1,1,2,2,4,4,5,5] 
+    Acount = 0
+    Bcount = 0
+    Ccount = 0
+    
     for i in range(len(answers)):
-        if A[i%len(A)] == answers[i]:
-            Acnt +=1
-        if B[i%len(B)] == answers[i]:
-            Bcnt +=1
-        if C[i%len(C)] == answers[i]:
-            Ccnt +=1    
-    if max(Acnt,Bcnt,Ccnt) == Acnt:
+        if answers[i] == A[i%len(A)]:
+            Acount += 1
+        if answers[i] == B[i%len(B)]:
+            Bcount += 1
+        if answers[i] == C[i%len(C)]:
+            Ccount += 1
+    if max(Acount,Bcount,Ccount) == Acount:
         answer.append(1)
-    if  max(Acnt,Bcnt,Ccnt) == Bcnt:
-        answer.append(2)   
-    if max(Acnt,Bcnt,Ccnt) == Ccnt:
-        answer.append(3)        
+    if max(Acount,Bcount,Ccount) == Bcount:
+        answer.append(2)
+    if max(Acount,Bcount,Ccount) == Ccount:
+        answer.append(3)
     return answer
