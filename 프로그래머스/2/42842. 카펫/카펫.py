6a59d862b0e8903903203366e0dfca14dd6b82c1
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-04-28 23:18:42 +0900

    [level 2] Title: 카펫, Time: 156.49 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,6 @@
 def solution(brown, yellow):
-    for a in range(3, brown+yellow+1): #가로
-        for b in range(3, a+1): #세로
-            if a * b== brown+yellow:
-                if yellow == (a-2) * (b-2):
-                     return [a,b]
-
+    for a in range(3, brown+yellow+1):
+        for b in range(3, a+1):
+            if a*b == brown+yellow:
+                if (a-2)*(b-2) == yellow:
+                    return [a,b]
