74b3514161caac3b46ce0e89f3ee210efc606412
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-01 17:40:39 +0900

    Time: 292 ms (25.05%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,15 +1,5 @@
-# Write your MySQL query statement below
-/**
 select (
-    select distinct salary SecondHighestSalary
-    from (
-    select  salary, dense_rank() over(order by salary desc) SecondHighestSalary
-    from Employee
-    order by salary desc  ) T
-    where SecondHighestSalary=2 ) SecondHighestSalary
-**/
-select (
-select distinct salary SecondHighestSalary
+select distinct salary
 from Employee
 order by salary desc
-limit 1 offset 1 ) SecondHighestSalary
+limit 1 offset 1) SecondHighestSalary
