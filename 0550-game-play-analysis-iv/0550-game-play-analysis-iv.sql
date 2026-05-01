84b8d46d970458ed08fdd368438d1400eb4209d5
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-02 01:15:40 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -5,6 +5,15 @@ select player_id , min(event_date) first_date
 from Activity
 group by player_id
 )
-select round(count(distinct F.player_id)/ count(distinct A.player_id),2) fraction 
-from Activity A
-left join  first F on A.player_id = F.player_id and A.event_date = F.first_date + interval 1 day
+select round(count(A.player_id)/ count(F.player_id),2) fraction 
+from first F
+left join Activity A on A.player_id = F.player_id and A.event_date = F.first_date + interval 1 day
+/**
+select round(count(case when datediff(event_date, first_date) = 1 then 1 end) / count(distinct player_id), 2) as fraction
+from (
+  select player_id,
+         event_date,
+         min(event_date) over (partition by player_id) as first_date
+  from Activity
+) t;
+**/
