f2d381f2f9d18cfa2fdb7a211b9b629ea34fed35
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-01 18:37:16 +0900

    Time: 1022 ms (10.02%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,7 +1,10 @@
-# Write your MySQL query statement below
 /**
-이메일 같은거 비교해서 중복된거 있으면 id가 더 큰행 삭제
-**/
+select id, email
+from (
+select id, email
+        , row_number() over(partition by email order by id) rn
+from Person) T
+where rn=1 **/
 DELETE p1 FROM Person p1
 JOIN Person p2 
 ON p1.email = p2.email AND p1.id > p2.id;
