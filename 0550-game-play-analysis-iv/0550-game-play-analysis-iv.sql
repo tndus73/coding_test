72b8501445bd6c40be6346b4b0dd53f93478a574
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-02 01:08:22 +0900

    Time: 1435 ms (5.9%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,14 +1,10 @@
-# Write your MySQL query statement below
 with first
 as
 (
-select player_id, min(event_date) first_date
+select player_id , min(event_date) first_date
 from Activity
 group by player_id
 )
-select round(count(distinct A.player_id) / (select count(distinct player_id)  from Activity),2) fraction
+select round(count(distinct F.player_id)/ count(distinct A.player_id),2) fraction 
 from Activity A
-left join first F on A.player_id = F.player_id
-where datediff(event_date,first_date)=1
-
-
+left join  first F on A.player_id = F.player_id and A.event_date = F.first_date + interval 1 day
