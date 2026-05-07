40d0ceb9cda96446b8cb6c1164cb93d458690264
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-07 15:29:10 +0900

    [Passed] 문자열의 특정 위치 찾기 2 (47ms, 16MB)

--- trail0/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
+++ trail0/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
@@ -1,9 +1,8 @@
 text = str(input())
 arr = ["apple", "banana", "grape", "blueberry", "orange"]
-arr2 = []
+count = 0
 for i in arr:
     if i[2] == text or i[3] == text:
-        arr2.append(i)
-for i in arr2:
-    print(i)
-print(len(arr2))
+        print(i)
+        count +=1
+print(count)
