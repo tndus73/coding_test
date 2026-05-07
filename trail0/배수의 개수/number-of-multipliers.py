1726de15ad6ba4bbaa636237a3cc594ce3385a98
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-07 15:22:59 +0900

    [Passed] 배수의 개수 (53ms, 16MB)

--- trail0/배수의 개수/number-of-multipliers.py
+++ trail0/배수의 개수/number-of-multipliers.py
@@ -1,14 +1,10 @@
-arr = []
-cnt3 = 0
-cnt5 = 0
-
+count1=0
+count2=0
 for _ in range(10):
-    arr.append(int(input()))
+    n=int(input())
+    if(n%3==0):
+        count1+=1
+    if(n%5==0):
+        count2+=1
 
-for i in arr:
-    if i%3 == 0:
-        cnt3 += 1
-    if i%5 ==0:
-        cnt5 += 1
-print(cnt3, cnt5)
-    
+print(count1, count2)
