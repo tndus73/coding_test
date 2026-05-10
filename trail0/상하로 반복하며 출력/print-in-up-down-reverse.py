fa0e6e54c3f2c2da86622a060a48d528f2e3557b
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-10 12:41:05 +0900

    [Passed] 상하로 반복하며 출력 (43ms, 16MB)

--- trail0/상하로 반복하며 출력/print-in-up-down-reverse.py
+++ trail0/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -1,9 +1,18 @@
 N = int(input())
+arr = [[0 for i in range(N)] for i in range(N)]
 
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        if j % 2 == 1:
-            print(i, end='')
-        else:
-            print(N - i + 1, end='')
+for i in range(N):
+    cnt = 1
+    if i % 2 == 0:
+        for j in range(N):
+            arr[j][i] = cnt
+            cnt +=1
+    else:
+        for j in range(N-1,-1,-1):
+            arr[j][i] = cnt
+            cnt +=1            
+
+for i in range(N):
+    for j in range(N):
+        print(arr[i][j], end='')
     print()
