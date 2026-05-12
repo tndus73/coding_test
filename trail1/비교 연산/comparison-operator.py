7d4f68b9bf5e3fa00988dc9b80434b7236251151
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-12 15:20:28 +0900

    [Passed] 비교 연산 (48ms, 16MB)

---
+++ trail1/비교 연산/comparison-operator.py
@@ -0,0 +1,8 @@
+A, B = map(int, input().split())
+
+print(1) if A>=B else print(0)
+print(1) if A>B else print(0)
+print(1) if B>=A else print(0)
+print(1) if B>A else print(0)
+print(1) if B==A else print(0)
+print(1) if A!=B else print(0)
