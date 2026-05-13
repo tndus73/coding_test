37df50a4b7dcd93276c50141b3e602b6638655a4
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-14 02:12:22 +0900

    [Passed] 출석 부르기 2 (43ms, 16MB)

---
+++ trail1/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    N = int(input())
+    if N == 1:
+        print('John')
+    elif N ==2:
+        print('Tom')
+    elif N ==3:
+        print('Paul')
+    elif N ==4:
+        print('Sam')
+    else:
+        print('Vacancy')
+        break
