18b24ad457b9b49ec7b21d16af6b0ed8051c0424
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-14 15:24:26 +0900

    [Passed] ab 사이에 있는 c (44ms, 16MB)

---
+++ trail1/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,10 @@
+a, b, c = map(int, input().split())
+answer = False
+for i in range(a, b+1):
+    if i % c == 0:
+        answer = True
+        break
+if answer == True:
+    print('YES')
+else:
+    print('NO')
