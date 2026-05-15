ad58ea988701c10035b7a9bea11a6a7f0d89072c
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-15 17:39:04 +0900

    [Passed] 별표 출력하기 6 (46ms, 16MB)

---
+++ trail1/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(N,0,-1):
+    for j in range(N-i):
+        print(' ' , end = ' ')
+    for j in range(2*i-1):
+        print('*' , end = ' ')
+    print()
+
+for i in range(2, N + 1):
+    for j in range(N - i):
+        print(' ', end=' ')
+    for j in range(2 * i - 1):
+        print('*', end=' ')
+    print()
