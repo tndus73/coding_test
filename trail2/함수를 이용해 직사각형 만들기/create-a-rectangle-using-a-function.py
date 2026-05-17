d94b6f836f1edd8ebccb5956cbbcb45fb2c7cec5
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-17 17:05:43 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (68ms, 17MB)

---
+++ trail2/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+def squre(n,m):
+    for i in range(n):
+        for j in range(m):
+            print(1, end ='')
+        print()
+
+squre(n,m)
+# Please write your code here.
