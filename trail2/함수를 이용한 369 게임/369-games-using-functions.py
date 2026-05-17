7bbf397931d3c16a726305483119457e84ab3cc8
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-18 00:07:56 +0900

    [Passed] 함수를 이용한 369 게임 (79ms, 18MB)

--- trail2/함수를 이용한 369 게임/369-games-using-functions.py
+++ trail2/함수를 이용한 369 게임/369-games-using-functions.py
@@ -4,12 +4,12 @@ def TF369(n):
     return '3' in str(n) or '6' in str(n) or '9' in str(n)
 
 def TF3(n):
-    return n % 3== 0 
+    return n % 3== 0 or TF369(n)
 
 cnt = 0
 
 for i in range(a, b+1):
-    if TF369(i) or TF3(i):
+    if TF3(i):
         cnt +=1
 print(cnt)
 # Please write your code here.
