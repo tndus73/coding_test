636f5bd295bacf4b49f3fa18a771bfd8acf94ea4
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-18 18:24:07 +0900

    [Passed] palindrome 여부 판단하기 (56ms, 19MB)

--- trail2/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ trail2/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -1,11 +1,11 @@
 A = input()
 
-def is_palin():
+def is_palin(A):
     if A[:] == A[::-1] :
         return True
     return False
 
-if is_palin():
+if is_palin(A):
     print('Yes')
 else:
     print('No')
