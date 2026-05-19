b4b67ff5b58531ae1cd6db9a5d8099c36b4c066e
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-19 14:27:54 +0900

    [Passed] 재귀함수를 이용한 최댓값 (51ms, 16MB)

--- trail2/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ trail2/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,7 +1,9 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-def maxT(arr):
-    return max(arr)
-print(maxT(arr))
+def maxT(a):
+    if a == 0:
+        return arr[0]
+    return max(maxT(a-1),arr[a])
+print(maxT(n-1))
 # Please write your code here.
