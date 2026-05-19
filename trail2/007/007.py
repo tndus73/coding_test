09eb4e0f49b8a5ca73b5595292680af3585334a7
tndus73 <91958363+tndus73@users.noreply.github.com>
2026-05-19 19:22:06 +0900

    [Passed] 007 (50ms, 16MB)

---
+++ trail2/007/007.py
@@ -0,0 +1,14 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+class code:
+    def __init__(self,secret_code,mmeeting_pointp,time):
+        self.ssecret_codec = secret_code
+        self.mmeeting_pointp = mmeeting_pointp
+        self.time = time
+
+code1 = code(secret_code,meeting_point,time)
+print(f'secret code : {secret_code}')
+print(f'meeting point : {meeting_point}')
+print(f'time : {time}') 
+# Please write your code here.
